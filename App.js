8938eb7ab66f19d6e7c8661882a52fa801866f33
shialax <74329916+shialax@users.noreply.github.com>
2024-11-10 22:10:10 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,27 +1,22 @@
-import "./App.css";
-import Carousel from "./Carousel";
-import photos from "./photos.js";
-
-/** App component, renders Carousel
- * 
- * Props: none
- * State: none
- * 
- * App --> Carousel
- */
-
 function App() {
-
-  // In real life, this data would probably come from an AJAX call.
-  // For our purposes, we're just importing from another file.
-  const carouselPhotos = photos;
-  const carouselTitle = "Shells from far-away beaches";
-
   return (
-    <div className="App">
-      <Carousel photos={carouselPhotos} title={carouselTitle} />
+    <div>
+      <Person
+        name="Homer"
+        age={38}
+        hobbies={["bowling", "watching tv", "drinking beer"]}
+      />
+      <Person name="Marge" age={34} hobbies={["painting", "gambling"]} />
+      <Person
+        name="Bart"
+        age={10}
+        hobbies={["skateboarding", "making prank calls"]}
+      />
+      <Person
+        name="Lisa"
+        age={8}
+        hobbies={["reading", "saxophone", "eating vegetables"]}
+      />
     </div>
   );
 }
-
-export default App;
